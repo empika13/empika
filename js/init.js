aa733c5cb439502e8a4cef4077cf4b6038a5363f
goldledoigt <gary@chewam.com>
2011-08-22 14:22:48 +0200

    add form

--- js/init.js
+++ js/init.js
@@ -1,30 +1,40 @@
-jQuery(function( $ ) {
-
-    var links = $('.links .link');
-
-    // setup animated anchor scroll
-    $('.links').localScroll({
-        target: '.container', // could be a selector or a jQuery object too.
-        queue:true,
-        duration:1000,
-        hash:true,
-        onBefore:function(e, anchor, $target) {
-            links.removeClass('selected');
-            $(e.currentTarget.parentNode).addClass('selected');
-        },
-        onAfter:function(anchor, settings) {}
-    });
-
-    // set selected menu item
+function setSelectedMenuItem() {
     var href, idx = 0,
-        h = window.location.hash;
+        h = window.location.hash,
+        links = $('.links .link');
+
+    links.removeClass('selected');
+
     if (h) {
         for (var i = 0, l = links.length; i < l; i++) {
             href = links[i].firstChild.attributes[0].value;
             if (href === h) idx = i;
         }
     }
+
     $(links[idx]).addClass('selected');
+}
+
+
+jQuery(function( $ ) {
+
+    // setup animated anchor scroll
+    $('.links, .container').localScroll({
+        target: '.container', // could be a selector or a jQuery object too.
+        queue:true,
+        duration:1000,
+        hash:true,
+        onBefore:function(e, anchor, $target) {
+            // $('.links .link').removeClass('selected');
+            // $(e.currentTarget.parentNode).addClass('selected');
+        },
+        onAfter:function(anchor, settings) {
+            setSelectedMenuItem();
+        }
+    });
+
+    // set selected menu item
+    setSelectedMenuItem();
 
     // setup carousel
     new flux.slider('#slider', {
