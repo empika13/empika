fcd76613f61a54162c4977f7bcc398c24d12082b
empika13 <empika13@gmail.com>
2011-08-22 14:17:03 +0200

    add sources

---
+++ js/init.js
@@ -0,0 +1,34 @@
+jQuery(function( $ ) {
+
+    var links = $('.links .link');
+
+    // setup animated anchor scroll
+    $('.links').localScroll({
+        target: '.container', // could be a selector or a jQuery object too.
+        queue:true,
+        duration:1000,
+        hash:true,
+        onBefore:function(e, anchor, $target) {
+            links.removeClass('selected');
+            $(e.currentTarget.parentNode).addClass('selected');
+        },
+        onAfter:function(anchor, settings) {}
+    });
+
+    // set selected menu item
+    var href, idx = 0,
+        h = window.location.hash;
+    if (h) {
+        for (var i = 0, l = links.length; i < l; i++) {
+            href = links[i].firstChild.attributes[0].value;
+            if (href === h) idx = i;
+        }
+    }
+    $(links[idx]).addClass('selected');
+
+    // setup carousel
+    new flux.slider('#slider', {
+        pagination: true
+    });
+
+});
